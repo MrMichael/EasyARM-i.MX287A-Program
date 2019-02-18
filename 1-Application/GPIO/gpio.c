0ca114e5e7bb99c0da00f48e40ad5cbabf41f93b
MrMicheal <1035285359@qq.com>
2019-02-18 11:08:10 +0000

    application

---
+++ 1-Application/GPIO/gpio.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+//文件操作需要的头文件
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <termios.h>
+#include <errno.h>
+
+#define direction_path	"/sys/devices/virtual/gpio/gpio68/direction"
+#define value_path	"/sys/devices/virtual/gpio/gpio68/value"
+
+int main(void)
+{
+	int fd = 0;
+	char buf[10];
+	
+	fd = open(direction_path,O_RDWR);
+	if(fd < 0) {
+		perror(direction_path);
+	}
+	lseek(fd, 0, SEEK_SET);
+	write(fd, "out", 3);
+	lseek(fd, 0, SEEK_SET);
+	read(fd, buf, 5);
+	printf("get value:%s \n", buf);
+	close(fd);
+	
+	fd = open(value_path,O_RDWR);
+	if(fd < 0) {
+		perror(value_path);
+	}
+	lseek(fd, 0, SEEK_SET);
+	write(fd, "1", 1);
+	close(fd);
+	
+	printf("ok\n");
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
